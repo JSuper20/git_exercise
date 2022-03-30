7e39c6cf56d60ebea39b017836f157d85775d8ad
JSuper20 <JC_jiangchao@126.com>
2022-03-31 00:44:09 +0800

    添加公共方法

---
+++ git_exercise/src/com/damahou/util/CommonUtil.java
@@ -0,0 +1,15 @@
+package com.damahou.util;
+
+/**
+ * @Program git_exercise
+ * @ClassName CommonUtil
+ * @Package com.damahou.util
+ * @Description TODO：CommonUtil
+ * @Author DaMaHou
+ * @CreateDate 2022-03-31 0:43
+ * @ModifyBy
+ * @ModifyDate
+ * @Version 1.0
+ */
+public class CommonUtil {
+}
